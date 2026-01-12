29da7a5c23905127025d7303ce6844b2097bd841
runAlgo <150834861+runAlgo@users.noreply.github.com>
2026-01-12 11:48:16 +0545

    Add initial configuration setup with environment variable loading

---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	MongoURI   string
+	MongoDB    string
+	ServerPort string
+}
+
+func Load() (Config, error) {
+
+	// godotenv.Load() reads .env and sets them into the process env
+	// os.getenv -> reads those values
+
+	if err := godotenv.Load(); err != nil {
+		return Config{}, fmt.Errorf("Failed to load .env")
+	}
+
+	mongoURI, err := extractEnv("MONGO_URI")
+	if err != nil {
+		return Config{}, nil
+	}
+	mongoDB, err := extractEnv("MONGO_DB_NAME")
+	if err != nil {
+		return Config{}, nil
+	}
+	port, err := extractEnv("PORT")
+		if err != nil {
+		return Config{}, nil
+	}
+
+	return Config{MongoURI: mongoURI, MongoDB: mongoDB, ServerPort: port}, nil
+}
+
+func extractEnv(key string) (string, error) {
+	val := os.Getenv(key)
+	if val == "" {
+		return "", fmt.Errorf("missing required env")
+	}
+	return val, nil
+}
